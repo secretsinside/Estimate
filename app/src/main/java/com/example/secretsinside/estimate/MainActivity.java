8f984466bfa70d4c2f65b803f73765f2c4f6e3fa
octacode <allblue.shasha@gmail.com>
2017-06-16 18:39:14 +0530

    Fixed the crash.

--- app/src/main/java/com/example/secretsinside/estimate/MainActivity.java
+++ app/src/main/java/com/example/secretsinside/estimate/MainActivity.java
@@ -8,17 +8,16 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText EditTextTransformerAmount= (EditText)findViewById(R.id.editTextTransformerAmount);
-    EditText TwoPercentOfAmount = (EditText)findViewById(R.id.twoPercentOfAmount);
-    EditText FourPercentOfAmount = (EditText)findViewById(R.id.fourPercentOfAmount);
-    EditText FortyPercentOfAmount = (EditText)findViewById(R.id.fortyPercentOfAmount);
-    Button ButtonCalculate = (Button)findViewById(R.id.buttonCalculate) ;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText EditTextTransformerAmount= (EditText)findViewById(R.id.editTextTransformerAmount);
+        final EditText TwoPercentOfAmount = (EditText)findViewById(R.id.twoPercentOfAmount);
+        EditText FourPercentOfAmount = (EditText)findViewById(R.id.fourPercentOfAmount);
+        EditText FortyPercentOfAmount = (EditText)findViewById(R.id.fortyPercentOfAmount);
+        Button ButtonCalculate = (Button)findViewById(R.id.buttonCalculate) ;
 
         ButtonCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
